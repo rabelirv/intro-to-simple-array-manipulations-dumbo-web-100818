4b407f535c7d94651e280b853ee67d8f1ca5bb33
Irving E Rabel <rabel606@gmail.com>
2018-09-22 15:20:06 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,8 +30,6 @@ def shift_with_args(ice_cream_brands)
 end
 
 def using_concat(my_favorite_things,more_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
   my_favorite_things.concat(more_favs)
 end
 
