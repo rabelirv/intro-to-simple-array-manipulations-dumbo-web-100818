c8a64b745eb37335237ce1acea232fd1c27c5954
Irving E Rabel <rabel606@gmail.com>
2018-09-22 15:19:11 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,6 @@ great_hits_of_the_nineties.pop
 end
 
 def pop_with_args(chars_in_game_of_thrones)
-   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
    chars_in_game_of_thrones.pop(2)
 end
 
